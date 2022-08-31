9799c84b68c6ee119106b50056e418ce70d585b5
Eliachar Feig <efeig15@gmail.com>
2022-08-31 20:34:54 +0300

    Adjust active buttons

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -27,6 +27,18 @@
 				window.setTimeout(function() {
 					$body.removeClass('is-loading');
 				}, 100);
+
+			[...document.querySelectorAll(".control")].forEach(button => {
+				button.addEventListener("click", function() {
+					document.querySelector(".active-btn").classList.remove("active-btn");
+					this.classList.add("active-btn");
+					document.querySelector(".active").classList.remove("active");
+					document.getElementById(button.dataset.id).classList.add("active");
+				})
+			});
+			document.querySelector(".theme-btn").addEventListener("click", () => {
+				document.body.classList.toggle("light-mode");
+			})
 			});
 
 		// Fix: Placeholder polyfill.
